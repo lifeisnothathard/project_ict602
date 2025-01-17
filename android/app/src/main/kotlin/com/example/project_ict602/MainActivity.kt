dd12886dd1cb7c8944bc10bf38993350229a21c4
Shafiqah24 <2023637684@student.uitm.edu.my>
2025-01-17 12:14:05 +0800

    First try

---
+++ android/app/src/main/kotlin/com/example/project_ict602/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_ict602
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage ({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage> {
+  List<CameraDescription> cameras = [];
+  CameraController? cameraController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setupCameraController();
+  }
+
+  @override
+  void dispose() {
+    //dispose camera controller
+    cameraController?.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: ui(),
+    );
+  }
+
+Widget ui() {
+   if (cameraController == null || !cameraController!.value.isInitialized);
+}
+
+  Future<void> setupCameraController() async{
+    try{
+      //fetch available camera
+      final _cameras = await availableCameras();
+      if (_cameras.isNotEmpty) {
+        setState(() {
+          cameras = _cameras;
+          cameraController = CameraController(_cameras.first, ResolutionPreset.high,);
+        });
+        //initialize the camera controller
+        await cameraController!.initialize();
+        //update UI after initialization
+        setState(() {
+          
+        });
+       } else {
+        showError("No Camera available on this device");
+       }
+
+    } catch(e) {
+      showError("Error initializing camera: $e");
+    }
+  }
+  void showError(String message) {
+  showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text(message), actions: [TextButton(onPressed: ()=> Navigator.of(context).pop, child: Text("Close"),)],),);
+}
+}
+
+
